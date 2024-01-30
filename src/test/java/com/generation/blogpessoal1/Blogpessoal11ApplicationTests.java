567ad69b559e939a21599929a04e5f27955bb72e
samuelferreira25 <samuel.ferreira2014.sf@gmail.com>
2024-01-30 14:18:44 -0300

    Blog Pessoal

---
+++ src/test/java/com/generation/blogpessoal1/Blogpessoal11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.blogpessoal1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blogpessoal11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
